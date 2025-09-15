b4ba58c9a5438418451df9c7e670646501b071e5
Izammohamed <izammohamed333@gmail.com>
2025-09-16 01:54:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Contact form alert
+document.getElementById("contactForm").addEventListener("submit", function(event){
+    event.preventDefault();
+    alert("Thank you for contacting me! I will reply soon.");
+});
